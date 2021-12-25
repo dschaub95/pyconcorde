9bf39c2c91dc954d85e499d20c275f4817b6d86b
dschaub95 <schaub.darius@gmail.com>
2021-12-25 16:01:31 +0100

    added test set generation script

---
+++ generate_test_sets_pomo.sh
@@ -0,0 +1,7 @@
+python generate_tsp_pomo.py --num_samples=10000 --num_nodes=100
+python generate_tsp_pomo.py --num_samples=10000 --num_nodes=50
+python generate_tsp_pomo.py --num_samples=10000 --num_nodes=20
+python generate_tsp_pomo.py --num_samples=128 --num_nodes=200
+python generate_tsp_pomo.py --num_samples=128 --num_nodes=300
+python generate_tsp_pomo.py --num_samples=128 --num_nodes=400
+python generate_tsp_pomo.py --num_samples=128 --num_nodes=500
